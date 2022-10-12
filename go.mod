b9907fae9059df4c24471e7535b83bba20ce5a4a
Nicole Hubbard <code@nicole.dev>
2022-10-12 00:03:55 +0000

    Add goose, version and viper helpers
    Signed-off-by: Nicole Hubbard <code@nicole.dev>

--- go.mod
+++ go.mod
@@ -2,9 +2,42 @@ module go.infratographer.com/x
 
 go 1.19
 
-require go.uber.org/zap v1.23.0
+require (
+	github.com/cockroachdb/cockroach-go/v2 v2.2.16
+	github.com/lib/pq v1.10.7
+	github.com/pressly/goose/v3 v3.7.0
+	github.com/spf13/cobra v1.6.0
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.13.0
+	go.uber.org/zap v1.23.0
+)
 
 require (
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ goosex/cobra.go
@@ -0,0 +1,84 @@
+// Copyright 2022 The Infratographer Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package goosex
+
+import (
+	"io/fs"
+
+	_ "github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgx" // crdb retries and postgres interface
+	_ "github.com/lib/pq"                                   // Register the Postgres driver.
+	"github.com/pressly/goose/v3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.infratographer.com/x/zapx"
+	"go.uber.org/zap"
+)
+
+var logger *zap.SugaredLogger
+
+// RegisterCobraCommand will add a migrate command to the cobra command provided
+// that provides a wrapper for running goose commands.
+func RegisterCobraCommand(cmd *cobra.Command, setupFunc func()) {
+	cmd.AddCommand(&cobra.Command{
+		Use:   "migrate <command> [args]",
+		Short: "Manage database schema migrations",
+		Long: `Migrate provides a wrapper around the "goose" migration tool.
+
+Commands:
+up                   Migrate the DB to the most recent version available
+up-by-one            Migrate the DB up by 1
+up-to VERSION        Migrate the DB to a specific VERSION
+down                 Roll back the version by 1
+down-to VERSION      Roll back to a specific VERSION
+redo                 Re-run the latest migration
+reset                Roll back all migrations
+status               Dump the migration status for the current DB
+version              Print the current version of the database
+create NAME [sql|go] Creates new migration file with the current timestamp
+fix                  Apply sequential ordering to migrations
+	`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			setupFunc()
+			migrate(args[0], args[1:])
+		},
+	})
+}
+
+func migrate(command string, args []string) {
+	db, err := goose.OpenDBWithDriver("postgres", viper.GetString("db.uri"))
+	if err != nil {
+		logger.Fatalw("failed to open DB", "error", err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			logger.Fatalw("failed to close DB", "error", err)
+		}
+	}()
+
+	if err := goose.Run(command, db, "migrations", args...); err != nil {
+		logger.Fatalw("migrate command failed", "command", command, "error", err)
+	}
+}
+
+func SetBaseFS(fsys fs.FS) {
+	goose.SetBaseFS(fsys)
+}
+
+func SetLogger(l *zap.SugaredLogger) {
+	logger = l.Named("goose")
+	goose.SetLogger(zapx.NewGooseLogger(logger))
+}
