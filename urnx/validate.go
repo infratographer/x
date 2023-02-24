6c6aeea887140ff0a7fbda616d6b75d754635a10
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-02-24 13:53:13 -0500

    regex more smarter
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- urnx/validate.go
+++ urnx/validate.go
@@ -4,16 +4,24 @@ import (
 	"regexp"
 )
 
+var (
+	nsrx *regexp.Regexp
+	rtrx *regexp.Regexp
+)
+
+func init() {
+	nsrx = regexp.MustCompile("^[A-Za-z0-9-]{1,30}$")
+	rtrx = regexp.MustCompile("^[A-Za-z0-9-]{1,255}$")
+}
+
 func validateNamespace(namespace string) bool {
-	r := regexp.MustCompile("^[A-Za-z0-9-]{1,30}$")
-	rx := r.MatchString(namespace)
+	rx := nsrx.MatchString(namespace)
 
 	return rx
 }
 
 func validateResourceType(name string) bool {
-	r := regexp.MustCompile("^[A-Za-z0-9-]{1,255}$")
-	rx := r.MatchString(name)
+	rx := rtrx.MatchString(name)
 
 	return rx
 }
