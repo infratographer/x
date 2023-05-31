85a03aeb8c45356328c766fcdd4413f76b821d07
Nicole Hubbard <code@nicole.dev>
2023-05-31 12:34:44 -0400

    Update license headers in files (#91)
    Signed-off-by: Nicole Hubbard <code@nicole.dev>

--- echojwtx/doc.go
+++ echojwtx/doc.go
@@ -1,3 +1,7 @@
+// Copyright Infratographer, Inc. and/or licensed to Infratographer, Inc. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 // Package echojwtx implements echo-jwt middleware with additional helpers
 // for setting the JWT Subject as an actor context value.
 package echojwtx
