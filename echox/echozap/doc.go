7dca3e53dd1e99c9a162e4fde0527caa486b72fa
Nicole Hubbard <code@nicole.dev>
2023-05-31 15:54:07 +0000

    Update license headers in files
    Signed-off-by: Nicole Hubbard <code@nicole.dev>

--- echox/echozap/doc.go
+++ echox/echozap/doc.go
@@ -1,2 +1,6 @@
+// Copyright Infratographer, Inc. and/or licensed to Infratographer, Inc. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 // Package echozap implements an echo.Logger interface as well as an echo Middleware to log requests.
 package echozap
--- events/publisher.go
+++ events/publisher.go
@@ -1,3 +1,7 @@
+// Copyright Infratographer, Inc. and/or licensed to Infratographer, Inc. under one
+// or more contributor license agreements. Licensed under the Elastic License 2.0;
+// you may not use this file except in compliance with the Elastic License 2.0.
+
 package events
 
 import (
